6eef3f17decdc40cbbad8b0620f6e5634cdb4a44
yc.zhang <zhangyc@qq.com>
2016-06-15 22:05:35 +0800

    Deprecated class StDevUtil

--- src/main/java/me/yczhang/math/StDevUtil.java
+++ src/main/java/me/yczhang/math/StDevUtil.java
@@ -3,11 +3,7 @@ package me.yczhang.math;
 /**
  * Created by YCZhang on 1/4/16.
  */
+@Deprecated
 public class StDevUtil {
 
-//	public static double calc(Collection<? extends Number> data) {
-//		for (Number n : data) {
-//			n.
-//		}
-//	}
 }
